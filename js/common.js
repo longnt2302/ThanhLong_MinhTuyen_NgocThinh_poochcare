b5bbbe43df7884a90d0ff2e79704584527d1f592
NTLONG_VN <longnt2302@gmail.com>
2024-05-05 13:53:42 +0700

    update section videos

--- js/common.js
+++ js/common.js
@@ -33,4 +33,11 @@ $(document).ready(function(){
 
     $('.count_num').countUp();
 
+    const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
 });
