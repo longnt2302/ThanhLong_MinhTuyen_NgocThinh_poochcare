56613df4a4ccc75a91c05b3618d34a10fcae78cd
NTLONG_VN <longnt2302@gmail.com>
2024-05-05 23:32:54 +0700

    update page top & about

--- js/common.js
+++ js/common.js
@@ -12,11 +12,19 @@ $(document).ready(function(){
     };
     $(window).scroll(function() {
         var $height = $(window).scrollTop();
-      if($height > 30) {
+        // add class header
+        if( $height > 30 ) {
             $('#header').addClass('fixed');
         } else {
             $('#header').removeClass('fixed');
         }
+        // add class back to top
+        if( $height > 100 ) {
+            $('.back-to-top').addClass('show');
+        } else {
+            $('.back-to-top').removeClass('show');
+        }
+
     });
     $('.theme-toggle').clickToggle(
         function() {
@@ -33,11 +41,36 @@ $(document).ready(function(){
 
     $('.count_num').countUp();
 
-    const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+    $('#testimonials .testimonials_carousel').slick({
+        dots: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,                
+                dots: true
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  dots: false,
+                }
+              }
+          ]
+    });
 
 });
